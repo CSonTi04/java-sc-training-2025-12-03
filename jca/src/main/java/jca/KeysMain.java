5877f1e126181ff04b29955b603d6d1567b1fd78
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-12-03 11:22:22 +0100

    Add KeysMain class for generating and displaying AES keys in hex format

---
+++ jca/src/main/java/jca/KeysMain.java
@@ -0,0 +1,16 @@
+package jca;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+import javax.crypto.KeyGenerator;
+
+public class KeysMain {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        var keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256); // 128, 192, or 256 bits - alapból erős random, de aparméterezhető is
+        var secretKey = keyGenerator.generateKey();
+        var hexEncoded = HexFormat.of();
+        var formattedHex = hexEncoded.formatHex(secretKey.getEncoded());
+        System.out.println("Generated AES Key (Hex): " + formattedHex);
+    }
+}
