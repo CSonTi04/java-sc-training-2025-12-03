29f2c21f6fa2fce86e17817784ccec9adff05c5f
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-12-05 14:59:27 +0100

    Add additional security filter chain for actuator endpoints

--- employees-standalone-form-spring-security/src/main/java/employees/SecurityConfig.java
+++ employees-standalone-form-spring-security/src/main/java/employees/SecurityConfig.java
@@ -2,6 +2,7 @@ package employees;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,9 +20,12 @@ public class SecurityConfig {
         return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 
+    //lehet több security fitler chain
     @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
+        http.securityMatcher("/login", "/", "/employees", "/create-employee","/logout","/default-ui-css") //ezekre az útvonalakra érvényes ez a security filter chain
+                .authorizeHttpRequests(
                         //sorrend számít!
                         //először a konkrétabb szabályok jönnek, aztán az általánosabbak
                         registry ->
@@ -30,15 +34,31 @@ public class SecurityConfig {
                                         //hogy lehet ezt a hibát kivédeni?
                                         //írjunk rá automatizált teszteket, és ellenőrizzük, hogy a megfelelő szerepkörrel csak a megfelelő funkciók érhetők el
                                         //.requestMatchers("/**").hasRole("USER")//itt vigyázzunk, hogy ne legyen /**, mert akkor minden engedélyezve lesz a USER-nek
-                                        .requestMatchers("/","/employees").hasRole("USER")//itt vigyázzunk, hogy ne legyen /**, mert akkor minden engedélyezve lesz a USER-nek
+                                        .requestMatchers("/", "/employees").hasRole("USER")//itt vigyázzunk, hogy ne legyen /**, mert akkor minden engedélyezve lesz a USER-nek
                                         .requestMatchers("/create-employee").hasRole("ADMIN")
                                         .anyRequest().denyAll()
                 )
                 //turn this off and set username to this:
                 //<!-- <script src="http://127.0.0.1:5500/server/hello.js"></script> -->
-                .headers(headers -> headers.contentSecurityPolicy( policy -> policy.policyDirectives("script-src 'self'")))
+                .headers(headers -> headers.contentSecurityPolicy(policy -> policy.policyDirectives("script-src 'self'")))
                 .formLogin(Customizer.withDefaults())
                 .logout(Customizer.withDefaults());
         return http.build();
     }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorFitlerChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/actuator/**")
+                .authorizeHttpRequests(
+                        registry ->
+                                registry.anyRequest().hasRole("ADMIN")
+                )
+                .headers(headers -> headers.contentSecurityPolicy(policy -> policy.policyDirectives("script-src 'self'")))
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+        return http.build();
+    }
+
+
 }
