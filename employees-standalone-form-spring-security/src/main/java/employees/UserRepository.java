f2fa5f740cd01726da0f3343cde12cd6cc301acb
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-12-05 13:05:14 +0100

    Refactor findUserByUsername query to enhance security and optimize data fetching

--- employees-standalone-form-spring-security/src/main/java/employees/UserRepository.java
+++ employees-standalone-form-spring-security/src/main/java/employees/UserRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    //itt is fel lehet használni a SPEL-t és a security-s dolgokat - spring data jpa integráció, így lehet spórolni a lekérdezésekkel
+    //@Query("select distinct u from User u left join fetch u.authorities where u.username = :username and e.user =?#{authentication.name}}")
     @Query("select distinct u from User u left join fetch u.authorities where u.username = :username")
     Optional<User> findUserByUsername(String username);
 }
