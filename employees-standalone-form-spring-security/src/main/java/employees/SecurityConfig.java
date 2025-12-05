39f6dd589d5665e5a1082a743576d11caac78588
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-12-05 11:42:56 +0100

    Restrict user access to root path in SecurityConfig

--- employees-standalone-form-spring-security/src/main/java/employees/SecurityConfig.java
+++ employees-standalone-form-spring-security/src/main/java/employees/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
                                         //egy kis sérülékenység: minden felhasználó eléri az összes alkalmazás funkciót
                                         //hogy lehet ezt a hibát kivédeni?
                                         //írjunk rá automatizált teszteket, és ellenőrizzük, hogy a megfelelő szerepkörrel csak a megfelelő funkciók érhetők el
-                                        .requestMatchers("/**").hasRole("USER")//itt vigyázzunk, hogy ne legyen /**, mert akkor minden engedélyezve lesz a USER-nek
+                                        //.requestMatchers("/**").hasRole("USER")//itt vigyázzunk, hogy ne legyen /**, mert akkor minden engedélyezve lesz a USER-nek
+                                        .requestMatchers("/").hasRole("USER")//itt vigyázzunk, hogy ne legyen /**, mert akkor minden engedélyezve lesz a USER-nek
                                         .requestMatchers("/create-employee").hasRole("ADMIN")
                                         .anyRequest().denyAll()
                 )
